ebb75a6bba5c81bb9caefec743cb7ed5ee6e9d83
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-11 21:30:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 # Our Ruby program executes so quickly that it doesn't actually count down at the speed of one second per number. See if you can make the loop pause for one second each trip around (hint). Write this in a new method called countdown_with_sleep that also takes one integer argument for the countdown.
 
 def countdown(integer)
-  while integer 
-    puts " integer SECOND(S)!"
+  while integer > 0 
+    puts " #{integer} SECOND(S)!"
     integer -=1 
 end   
  return "HAPPY NEW YEAR!"
